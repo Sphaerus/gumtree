c17a945a7ba8be99eca7cb4d285c8f3599257a03
Sphaerus <wyfsm1@gmail.com>
2013-11-02 17:08:33 +0100

    has many association between poster and description fields + getting rid of old code

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,7 +1,7 @@
 class Poster < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  has_many :description_fields
   
   validates :description, presence: true
-  validates :kind_id, presence: true
 end
