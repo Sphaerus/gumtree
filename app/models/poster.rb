9e4c5860fa1fac081e4ea3057dde753651f5d42b
Sphaerus <wyfsm1@gmail.com>
2013-11-03 13:01:22 +0100

    poster model changes
    has many fields of all kind.
     accepts nested attr for them
    methods for building and listing fields for new poster based on a
    category

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,7 +1,33 @@
 class Poster < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
-  has_many :description_fields
+  has_many :text_fields
+  has_many :string_fields
+  has_many :integer_fields
+  has_many :float_fields
+  has_many :date_fields
+    
+  accepts_nested_attributes_for :float_fields
+  accepts_nested_attributes_for :integer_fields
+  accepts_nested_attributes_for :text_fields
+  accepts_nested_attributes_for :string_fields
+  accepts_nested_attributes_for :date_fields
   
   validates :description, presence: true
+  
+  def build_fields_based_on_category
+    category.fields.each do |field|
+      self.send("#{field.kind}_fields").build
+    end    
+  end  
+  
+  def list_fields
+    fields = []
+    self.text_fields.each { |field| fields << field }
+    self.string_fields.each { |field| fields << field }
+    self.integer_fields.each { |field| fields << field }
+    self.date_fields.each { |field| fields << field }
+    self.float_fields.each { |field| fields << field }
+    fields
+  end  
 end
