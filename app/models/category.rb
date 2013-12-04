28c8e206fad7806d5c9ac0068ebd7765f36d799a
Sphaerus <wyfsm1@gmail.com>
2013-12-04 22:44:46 +0100

    Only categories with no children are now possible to choose while creating a poster

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,22 @@ class Category < ActiveRecord::Base
   belongs_to :parent_category, class_name: "Category"
   
   scope :without_parent, -> { where(category_id: nil)}
+  
+  def subcategories_with_children
+    if self.subcategories.empty?
+      self.id
+    else
+      category_ids = self.subcategories.map do |subcategory|
+        subcategory.subcategories_with_children
+      end
+      category_ids.flatten
+    end    
+  end  
+  
+  def self.all_childless_categories
+    childless_categories = Category.without_parent.map do |category|
+      category.subcategories_with_children
+    end
+    self.where("id IN (?)", childless_categories.flatten)
+  end
 end
