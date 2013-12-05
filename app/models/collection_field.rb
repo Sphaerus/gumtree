c5fca5205196752a9d967cc5e257f39d4a1689bc
Sphaerus <wyfsm1@gmail.com>
2013-12-05 09:27:48 +0100

    its easier to get to the collection from collectionField

--- app/models/collection_field.rb
+++ app/models/collection_field.rb
@@ -3,4 +3,8 @@ class CollectionField < ActiveRecord::Base
   
   belongs_to :poster
   belongs_to :source, class_name: "Field", foreign_key: "field_id"
+  
+  def array
+    source.collection.array
+  end
 end
