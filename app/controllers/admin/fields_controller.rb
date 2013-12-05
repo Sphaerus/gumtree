ef90b936f40266a509d72bab4609a05d78acc1cc
Sphaerus <wyfsm1@gmail.com>
2013-12-05 10:46:19 +0100

    fields got their own view

--- app/controllers/admin/fields_controller.rb
+++ app/controllers/admin/fields_controller.rb
@@ -1,22 +1,30 @@
 class Admin::FieldsController < AdminController
-  before_action :set_category, only: [:create, :destroy]
+  before_action :set_category, only: [:index, :new, :create, :destroy]
   before_action :set_field, only: [:destroy]
+  
+  def index
+    @fields = @category.fields
+  end
+  
+  def new
+    @field = @category.fields.build
+  end
   
   def create    
     @field = @category.fields.build(field_params)
 
     respond_to do |format|
       if @field.save
-        format.html { redirect_to [:admin, @category]}
+        format.html { redirect_to admin_category_fields_path(@category)}
       else
-        format.html { redirect_to [:admin, @category]}
+        format.html { render action: "new"}
       end  
     end    
   end     
   
   def destroy
     @field.destroy
-    redirect_to [:admin, @category]
+    redirect_to [:admin, @category, :fields]
   end  
   
   private
